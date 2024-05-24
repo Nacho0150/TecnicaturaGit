9fc03c79635689aa9bf0da9a72f1963a7c049fa3
Nacho0150 <ignacioibanez01@gmail.com>
2024-05-24 13:31:50 -0300

    Ingresamos nuestra carpeta Java al commit

---
+++ Java/Leccion1/HolaMundo/.idea/sonarlint/issuestore/index.pb
@@ -0,0 +1,7 @@
+
+7
+pom.xml,4\4\442292b8a7efeabbe4cc176709b833b1792140ec
+X
+(src/main/java/org/example/HolaMundo.java,b\b\bb67c53a7197f50594e9b57895330d40500443bc
+Y
+)src/main/java/org/example/libro/Main.java,c\4\c44ed97b6a8d46cc9b498803b766c17bb2c60a9a
---
+++ Java/Leccion1/HolaMundo/src/main/java/org/example/HolaMundo.java
@@ -0,0 +1,156 @@
+package org.example;
+
+import java.util.Scanner;
+
+public class HolaMundo {
+    public static void main(String[] args) {
+        /*System.out.println("Hola Mundo desde Java");
+
+        int miVariable = 10;
+        System.out.println(miVariable);
+        miVariable = 5;
+        System.out.println(miVariable);
+        //Tipo String
+        String miVariableCadena = "Bienvenidos";
+        System.out.println(miVariableCadena);
+        miVariableCadena = "Sigamos creciendo en programación";
+        System.out.println(miVariableCadena);*/
+
+        //Var - inferencia de tipos en Java
+        /*var miVariableEntera2 = 10;
+        var miVariableCadena2 = "Seguimos estudiando";
+        System.out.println("miVariableEntera2 = " + miVariableEntera2);
+        System.out.println("miVariableCadena2 = " + miVariableCadena2);
+        //soutv + tab
+        //Para ejecutar Shift + F6 es la tecla para mayuscula
+        //Reglas para definir una variable en Java
+
+        var ususario = "Osvaldo";
+        var titulo = "Ingeniero";
+        var uninon = titulo + " " + ususario;
+        System.out.println("union = " + uninon);
+
+        var a = 8;
+        var b = 4;
+        System.out.println(ususario + (a + b));
+        //Ejercicio: Caracteres Especiales con Java
+        var nombre = "Natalia";
+        System.out.println("Nueva linea: \n" + nombre); // Diagonal inversa y letra n
+        System.out.println("Tabulador: \t" + nombre); //Tabulador un espacio para centrar
+        System.out.println("\t\t.:MENÚ:.");
+        System.out.println("Retroceso: \b\b" + nombre); //Caracter de Retroceso
+        System.out.println("Comillas Simples: \'" + nombre + "\'");
+        System.out.println("Comillas Dobles: \"" + nombre + "\"");*/
+
+        //Clase Scanner
+        /*Scanner entrada = new Scanner(System.in);
+        System.out.println("Digite su nombre: ");
+        var usuario2 = entrada.nextLine();
+        System.out.println("usuario2 = " + usuario2);
+        System.out.println("Escriba el titulo: ");
+        var titulo2 = entrada.nextLine();
+        System.out.println("Resultado: " + titulo2 + " " + usuario2);
+
+        byte numEnteroByte = (byte)127;
+        System.out.println("numEnteroByte = " + numEnteroByte);
+        System.out.println("Valor mínimo del Byte: " + Byte.MIN_VALUE);
+        System.out.println("Valor máximo del Byte: " + Byte.MAX_VALUE);
+
+        short numEnteroShort = (short)32768;
+        System.out.println("numEnteroShort = " + numEnteroShort);
+        System.out.println("Valor mínimo del Short: " + Short.MIN_VALUE);
+        System.out.println("Valor máximo del Short: " + Short.MAX_VALUE);
+
+        int numEnteroInt = 2147483647;
+        System.out.println("numEnteroInt = " + numEnteroInt);
+        System.out.println("Valor mínimo del Int: " + Integer.MIN_VALUE);
+        System.out.println("Valor máximo del Int: " + Integer.MAX_VALUE);
+
+        long numEnteroLong = 9223372036854775807L;
+        System.out.println("numEnteroLong = " + numEnteroLong);
+        System.out.println("Valor mínimo del Long: " + Long.MIN_VALUE);
+        System.out.println("Valor máximo del Long: " + Long.MAX_VALUE);*/
+
+        /*float numFloat = 3.4028235E38F;
+        System.out.println("numFloat = " + numFloat);
+        System.out.println("Valor mínimo del Float: " + Float.MIN_VALUE);
+        System.out.println("Valor máximo del Float: " + Float.MAX_VALUE);
+
+        double numDouble = 1.7976931348623157E308D;
+        System.out.println("numDouble = " + numDouble);
+        System.out.println("Valor mínimo del Double: " + Double.MIN_VALUE);
+        System.out.println("Valor máximo del Double: " + Double.MAX_VALUE);*/
+
+        //Interferencia de tipos var y tipos primitivos
+        /*var numEntero = 20; //Las literales sin punto automaticamente son de tipo int
+        System.out.println("numEntero = " + numEntero);
+        var numFloat = 10.0F; //Automaticamente el punto se transforma en tipo double
+        System.out.println("numFloat = " + numFloat);
+        var numDouble = 10.0;
+        System.out.println("numDouble = " + numDouble);*/
+
+        //Tipos primitivos char
+        /*char miVariableChar = 'a';
+        System.out.println("miVariableChar = " + miVariableChar);
+
+        char varCaracter = '\u0024'; //Indicamos a Java la asignación con el código unicode
+        System.out.println("varCaracter = " + varCaracter) ;
+        char varCaracterDecimal = 36; //Valor decimal del juego de caracteres unicode
+        System.out.println("varCaracterDecimal = " + varCaracterDecimal);
+        char varCaracterSimbolo = '$'; //Un caracter especial, podemos copiar y pegar desde unicode
+        System.out.println("varCaracterSimbolo = " + varCaracterSimbolo);
+
+        var varCaracter1 = '\u0024'; //Indicamos a Java la asignación con el código unicode
+        System.out.println("varCaracter1 = " + varCaracter1);
+        var varCaracterDecimal1 = (char)36; //Valor Entero y le asigna un tipo int|
+        System.out.println("varCaracterDecimall = " + varCaracterDecimal1);
+        var varCaracterSimbolo1 = '$'; //Un caracter especial, podemos copiar y pegar desde unicode
+        System.out.println("varCaracterSimbolo1 = " + varCaracterSimbolo1);
+
+        int varEnteroChar = '$';
+        System.out.println("varEnteroChar = " + varEnteroChar);
+        int caracterChar = 'b';
+        System.out.println("caracterChar = " + caracterChar);*/
+
+        //Tipos primitivos tipos booleanos
+        /*boolean varBool = true;
+        System.out.println("varBool = " + varBool);
+
+        if (varBool) {
+            System.out.println("varBool = " + varBool);
+        } else {
+            System.out.println("La bandera es roja");
+        }
+
+        //Algoritmo: ¿Es mayor de edad?
+        var edad = 30; //Literal tener presente la inferencia de tipos
+        //var adulto = edad >= 18; //Esta es una expresión booleana
+        if (edad >= 18) {
+            System.out.println("Eres mayor de edad");
+        } else {
+            System.out.println("Eres menor de edad");
+        }*/
+        //Conversión de tipos primitivos
+        /*var edad = Integer.parseInt("20");
+        System.out.println("edad = " + (edad + 1));
+        var valorPI = Double.parseDouble("3.1416");
+        System.out.println("valorPI = " + valorPI);*/
+
+        //Pedir un valor
+        var entrada = new Scanner(System.in);
+//        System.out.println("Digite su edad:");
+//        edad = Integer.parseInt(entrada.nextLine());
+//        System.out.println("edad = " + edad);
+
+        //Conversión de tipos primitivos en Java Parte 2
+        var edadTexto = String.valueOf(10);
+        System.out.println("edadTexto = " + edadTexto);
+
+        var fraseChar = "programadores".charAt(4);
+        System.out.println("fraseChar = " + fraseChar);
+
+        System.out.println("Digite un caracter: ");
+        fraseChar = entrada.nextLine().charAt(0);
+        System.out.println("fraseChar = " + fraseChar);
+    }
+}
