7ade1c9df0a56a79b22e671a1117bd17ed3def0a
Nacho0150 <ignacioibanez01@gmail.com>
2024-05-29 11:18:07 -0300

    Operadores en JavaScript y ejercicios

--- JavaScript/Leccion2/Leccion3/03-01-OperadoresJavaScript-UJS.js
+++ JavaScript/Leccion2/Leccion3/03-01-OperadoresJavaScript-UJS.js
@@ -6,3 +6,20 @@ if (num % 2 == 0) {
 } else {
     console.log("El número es impar")
 }
+
+// Ejercicio: es mayor de edad
+let edad = 18, adulto = 18;
+if(edad >= adulto){
+    console.log('Usted es una persona adulta');
+} else {
+    console.log('Usted es una persona menor de edad')
+}
+
+//Ejercicio: Dentro de un rango
+let dentroRango = 10; //Aquí vamos a ir cambiando el valor
+let valMin = 0, valMax = 10;
+if(dentroRango >= valMin && dentroRango <= valMax){
+    console.log('Esta dentro del rango establecido')
+} else {
+    console.log('Esta dentro del rango establecido')
+}
